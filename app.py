6c55b82815e44e5ecbc3e381326a7110f0d71de0
hoangxg4 <161178554+hoangxg4@users.noreply.github.com>
2024-03-16 19:44:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return 'Hello'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
